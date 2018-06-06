f3a7da85a6311fe890d3cd8b9d3ae2648196fd0a
wyang <wyang1117@msn.com>
2018-06-06 09:55:17 +0800

    添加时间格式化

--- application/admin/model/IndexModel.php
+++ application/admin/model/IndexModel.php
@@ -28,7 +28,9 @@ class IndexModel extends Model
 
     public function articleInfo(){
         $id = Session::get('id');
-        $article = Db::name('article')->where('authorId', $id)->select();
+        //使用FROM_UNIXTIME将create_time转换成年月日格式输出，FROM_UNIXTIME时MySql的方法
+        //field方法属于模型的连贯操作方法之一，主要目的是标识要返回或者操作的字段，可以用于查询和写入操作。
+        $article = Db::name('article')->where('authorId', $id)->field('FROM_UNIXTIME(create_time) as create_time,FROM_UNIXTIME(update_time) as update_time,author,authorId,content,id')->select();
         return ['code' => 1, 'data' => $article, 'msg' => '发表成功'];
     }
 }
