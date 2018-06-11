48982b3493e6e1cc51708d0ee24566daf92035e1
wyang <wyang1117@msn.com>
2018-06-11 09:59:41 +0800

    修改登录层数据查询方式

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -5,27 +5,36 @@
  * Date: 2018/4/16
  * Time: 11:39
  */
+
 namespace app\admin\controller;
 
 use app\admin\model\LoginModel;
 use think\Controller;
 use think\facade\Session;
 
-class Login extends Controller {
-    public function index(){
+class Login extends Controller
+{
+    public function index()
+    {
         return $this->fetch();
     }
+
     //登录
-    public function login(){
-        $phone= input('phone');
-        $password= input('password');
+    public function login()
+    {
+        $phone = input('phone');
+        $password = input('password');
         $loginInfo = new LoginModel();
-        $data = $loginInfo->getInfo($phone,$password);
+        $data = $loginInfo->getInfo($phone, $password);
+        $code = $data['code'];
+        if ($code == -1){
+            return json($data);
+        }
         $username = $data['username'];
         $id = $data['id'];
         //存储session
-        Session::set('username',$username);
-        Session::set('id',$id);
-        return json(['code' => 1,'msg' => $data['msg']]);
+        Session::set('username', $username);
+        Session::set('id', $id);
+        return json(['code' => 1, 'msg' => $data['msg']]);
     }
 }
--- application/admin/model/LoginModel.php
+++ application/admin/model/LoginModel.php
@@ -5,20 +5,26 @@
  * Date: 2018/4/16
  * Time: 11:39
  */
+
 namespace app\admin\model;
 
 use think\Model;
 use think\Db;
 
-class LoginModel extends Model {
-    public function getInfo($phone,$password){
-        $userInfo = Db::name('user')->where(['phone'=>$phone,'password'=>$password])->find();
-        $username = $userInfo['username'];
-        $id = $userInfo['id'];
-        if (empty($userInfo)){
-            return '登录失败';
-        }else{
-            return ['username'=>$username,'id'=>$id,'msg'=>"登录成功"];
+class LoginModel extends Model
+{
+    public function getInfo($phone, $password)
+    {
+        $dbUser = Db::name('user')->where('phone', $phone)->find();
+        $dbPassWord = Db::name('user')->where('password', $password)->find();
+        $username = $dbUser['username'];
+        $id = $dbUser['id'];
+        if ($dbUser == null) {
+            return ['code' => -1, 'msg' => "用户名不存在"];
         }
+        if ($dbPassWord == null) {
+            return ['code' => -1, 'msg' => "密码错误"];
+        }
+        return ['code' => 1, 'username' => $username, 'id' => $id, 'msg' => "登录成功"];
     }
 }
